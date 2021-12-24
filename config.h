14e6503d2a9f151f5c48deefbfa22608029f1825
Aamon-Magnusson <aamonm6@gmail.com>
2021-12-24 12:22:03 -0700

    Changed text colour in dmenu and dwm

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray4[]       = "#000000"; //"#eeeeee";
 static const char col_cyan[]        = "#ff00ff"; //"#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
--- options.sh
+++ options.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-cat /home/aamonm/Programming/AamonDwm/options.txt | dmenu -fn "monospace:size=15" -nb "#222222" -nf "#bbbbbb" -sb "#ff00ff" -sf "#eeeeee" -l 15
+cat /home/aamonm/Programming/AamonDwm/options.txt | dmenu -fn "monospace:size=15" -nb "#222222" -nf "#bbbbbb" -sb "#ff00ff" -sf "#000000" -l 15
