554f4812bcbb8b3af3399f2dbf796efbc69ff26e
Aamon Magnusson <aamonm6@gmail.com>
2022-01-23 12:26:26 -0700

    Making sure the setup works

--- setup.sh
+++ setup.sh
@@ -13,11 +13,13 @@ sudo cp $working_dir/CopyFiles/dwm.desktop /usr/share/xsessions/
 mkdir -p $HOME/.dwm
 cp $working_dir/CopyFiles/autostart.sh $HOME/.dwm/
 mkdir -p $HOME/.config/dunst
-cp $working_dir/CopyFiles/dunstrc $HOME/.config/dunst
+cp $working_dir/CopyFiles/dunstrc $HOME/.config/dunst/
 cp $working_dir/CopyFiles/AamonGTK3 $HOME/.themes/ -r
 cp $working_dir/CopyFiles/AamonIcons $HOME/.icons/ -r
 cp $working_dir/CopyFiles/Backgrounds $HOME/Desktop/ -r
+sudo rm -r /usr/AamonDwmScripts
 sudo cp $working_dir/AamonDwmScripts /usr/ -r
+mkdir -p $HOME/.weather
 
 echo "Install complete"
 
