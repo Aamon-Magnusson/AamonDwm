8fe890814ed03b8a370801691b402d1c874bd425
Aamon Magnusson <aamonm6@gmail.com>
2022-05-08 21:02:12 -0600

    fix?

--- install.sh
+++ install.sh
@@ -121,11 +121,11 @@ fileCopy () {
 	cp CopyFiles/autostart.sh $HOME/.dwm/
 	cp CopyFiles/Backgrounds $HOME/Desktop/ -r
 	sudo rm -r /usr/AamonDwmScripts
-	if [ $(expr $res) -lt 1000 ];then
+	if [ $(expr $res -tl 1000) ];then
 		sed -i "s|dmenu -l 45 -i -p \"Keybindings:\"|dmenu -l 20 -i -p \"Keybindings:\"|g" Scripts/dmenu-keybindings
 	fi
 	sudo cp Scripts /usr/AamonDwmScripts -r
-	if [ $(expr $res) -lt 1000 ];then
+	if [ $(expr $res -tl 1000) ];then
 		sed -i "s|dmenu -l 20 -i -p \"Keybindings:\"|dmenu -l 45 -i -p \"Keybindings:\"|g" Scripts/dmenu-keybindings
 	fi
 	cp CopyFiles/networkmanager-dmenu $HOME/.config/ -r
