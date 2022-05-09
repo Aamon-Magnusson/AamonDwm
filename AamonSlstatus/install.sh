376c908e4f07f3f589a8fd558485dfe720acf81e
Aamon Magnusson <aamonm6@gmail.com>
2022-05-08 20:39:29 -0600

    change AamonSlstatus for small screens

--- AamonSlstatus/install.sh
+++ AamonSlstatus/install.sh
@@ -3,6 +3,7 @@
 wired=$(ip a | grep "BROADCAST" | awk -F: '{print $2}' | grep 'en' | awk 'NR==1{print $1}')
 wireless=$(ip a | grep "BROADCAST" | awk -F: '{print $2}' | grep 'wl' | awk 'NR==1{print $1}')
 bat=$(ls /sys/class/power_supply | grep "BAT*" )
+res=$(xrandr | grep "*" | awk '{print $1}' | awk -Fx '{print $2}')
 
 #echo -e $wired
 #echo -e $wireless
@@ -11,8 +12,19 @@ sed -i "s/wlp4s0/$wireless/g" config.h
 sed -i "s/eno1/$wired/g" config.h
 sed -i "s/BAT0/$bat/g" config.h
 
+
+if [ "$res" == "720" ] || [ "$res" == "768" ];then
+	mv config.h large.h
+	mv small.h config.h
+fi
+
 sudo make -s install clean
 
 sed -i "s/$wireless/wlp4s0/g" config.h
 sed -i "s/$wired/eno1/g" config.h
 sed -i "s/$bat/BAT0/g" config.h
+
+if [ "$res" == "720" ] || [ "$res" == "768" ];then
+	mv config.h small.h
+	mv large.h config.h
+fi
