0faad10645c9f432081044187db931740abda3d3
Aamon Magnusson <aamonm6@gmail.com>
2022-08-25 17:13:31 -0600

    trying to have more colors

--- AamonDwm/colors/current.h
+++ AamonDwm/colors/current.h
@@ -1,9 +1,9 @@
 // dracula
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#282a36";
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#6272a4";
 static const char col_gray3[]       = "#f8f8f2";
-static const char col_gray4[]       = "#000000"; //"#eeeeee";
+static const char col_gray4[]       = "#282a36"; //"#eeeeee";
 static const char col_cyan[]        = "#bd93f9"; //"#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
