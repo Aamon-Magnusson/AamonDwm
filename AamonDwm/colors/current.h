26caea00b02cea7d13474ecef17a35d86e19dab2
Aamon Magnusson <aamonm6@gmail.com>
2022-12-30 14:44:06 -0700

    new dracula

--- AamonDwm/colors/current.h
+++ AamonDwm/colors/current.h
@@ -4,7 +4,7 @@ static const char col_gray1[]       = "#282a36";
 static const char col_gray2[]       = "#6272a4";
 static const char col_gray3[]       = "#f8f8f2";
 static const char col_gray4[]       = "#282a36"; //"#eeeeee";
-static const char col_cyan[]        = "#bd93f9"; //"#005577";
+static const char col_cyan[]        = "#ffb86c"; //"#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
